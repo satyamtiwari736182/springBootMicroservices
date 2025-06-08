5e9b98b0efae7ef279d5d0a2272304391cd3a5a8
Satyam Tiwari <satyamtiwari@Satyams-MacBook-Air.local>
2025-06-08 14:57:34 +0530

    config-client added

--- service1/src/main/java/com/service1/service1/Service1Application.java
+++ service1/src/main/java/com/service1/service1/Service1Application.java
@@ -1,15 +1,21 @@
 package com.service1.service1;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class Service1Application {
+public class Service1Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Service1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		System.out.println("Service 1 Application has started successfully!...");
 	}
 
 }
