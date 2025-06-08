fe193a0249acea1bb0f98257b22944af2071ecd4
Satyam Tiwari <satyamtiwari@Satyams-MacBook-Air.local>
2025-06-08 10:42:33 +0530

    initial

--- apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/example/apigateway/ApigatewayApplication.java
@@ -1,10 +1,17 @@
 package com.example.apigateway;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
-public class ApigatewayApplication {
+public class ApigatewayApplication implements CommandLineRunner {
+	@Override
+	public void run(String... args) {
+		System.out.println("API Gateway Application has started successfully!...");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApigatewayApplication.class, args);
